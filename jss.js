c6b8365f9e4dbaf809a32bf3d8a65c7a5292f11d
sly019 <deivermora19@gmail.com>
2014-07-03 02:22:53 -0600

    se añade pagina de carreras

--- jss.js
+++ jss.js
@@ -11,9 +11,39 @@ function Funcion_eliminar() {
 
 function Funcion_agregar() {
     if (confirm('Desea agregar un estudiante nuevo??')) {
-       open("../Estudiantes/formulario.html", "Sizewindow","width=1300,height=600,scrollbars=yes,toolbar=yes")
+       open("../Estudiantes/formulario.html", "Sizewindow","width=2500,height=1000,scrollbars=no,toolbar=no, Resizable=no")
         ;
     } else {
         document.location.href='../Estudiantes/estudiante.html';
     }
 }
+
+function Funcion_ver() {
+    if (confirm('Desea ver el estudiante seleccionado??')) {
+       open("../Estudiantes/ver.html", "Sizewindow","width=2500,height=1000,scrollbars=yes,toolbar=yes");
+    } else {
+        document.location.href='../Estudiantes/estudiante.html';
+    }
+}
+
+
+function Funcion_cancelar() {
+        window.close();
+    }
+
+
+function Funcion_agregar_est() {
+    alert  ("Se agregó el estudiante correctamente")
+    window.close();
+}
+
+function Funcion_editar_fin() {
+    alert("Se editó el estudiante correctamente")
+    window.close();
+}
+
+function Funcion_editar_est() {   
+    open("../Estudiantes/edicion.html", "Sizewindow","width=2500,height=1000,scrollbars=yes,toolbar=yes");
+}
+
+
